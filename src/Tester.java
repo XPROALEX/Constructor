0cac662a62b01bd3bd860864885c5793d8e98058
Alex <alex97av@gmail.com>
2023-07-27 17:48:36 +0200

    add house1 and house 2 variables

--- src/Tester.java
+++ src/Tester.java
@@ -1,5 +1,7 @@
 public class Tester {
     public static void main(String[] args){
         House house1,house2;
+        house1=new House("Via roma 15",3);
+        house2=new House("Viale lepanto 75",6);
     }
 }
