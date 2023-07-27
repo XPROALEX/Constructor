8f188907784932bffbba5b1ff21f769b77391a8f
Alex <alex97av@gmail.com>
2023-07-27 17:45:44 +0200

    Add House obj and constructor

--- src/House.java
+++ src/House.java
@@ -1,3 +1,10 @@
 public class House{
+    private String address;
+    private int numberOfFloors;
+    public House(String address,int numberOfFloors){
+        this.address=address;
+        this.numberOfFloors=numberOfFloors;
+        System.out.println("a new House object was created with the following data: \n"+"Address: "+address+"\nNumber of floors: "+numberOfFloors);
+    }
 
         }
